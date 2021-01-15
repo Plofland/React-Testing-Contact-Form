f106483921de9842706fbc22b1a23aca95117700
Plofland <ploflan@gmail.com>
2021-01-14 21:19:30 -0500

    All tests pass

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,33 +3,33 @@ import ContactForm from './ContactForm';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-test('renders without errors', async () => {
-  //Arrange
+test('inputs are valid and app renders without errors', () => {
+  //!Arrange
   render(<ContactForm />);
 
-  //Act
+  //!Act
   //1. Get the input fields
-  const firstNameInput = screen.queryByLabelText(/first name/i);
-  console.log(firstNameInput);
-  // const lastNameInput = screen.getByLabelText(/last name/i);
-  // const emailInput = screen.getByLabelText(/email/i);
+
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  // console.log(firstNameInput);
+
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  // console.log(lastNameInput);
+
+  const emailInput = screen.getByLabelText(/email/i);
   //2. Type values into the input fields
-  // userEvent.type(firstNameInput, 'Pal');
-  // userEvent.type(lastNameInput, 'Bond');
-  // userEvent.type(emailInput, 'something@gmail.com');
+  userEvent.type(firstNameInput, 'Pal');
+  userEvent.type(lastNameInput, 'Bond');
+  userEvent.type(emailInput, 'something@gmail.com');
 
   //3. Push the Submit button
-  // const submitInput = screen.getByRole();//inputs dont have a role
-  // userEvent.click(submitInput)
+  const submitInput = screen.getByTestId(/submit/i);
+  //inputs dont have a role
+  //but inputs with the type of submit(type="submit"), then it counts as a button
+  userEvent.click(submitInput);
 
-  //Assert
-  //1. First name on screen
-  // const newFirstName = await screen.findByText('Pal');
-  // expect(newFirstName).toBeInTheDocument();
-  //2. Last name on screen
-  // const newLastname = await screen.findByText('Bond');
-  // expect(newLastname).toBeInTheDocument();
-  //3. Email on screen
-  // const newEmail = await screen.findByText('something@gmail.com');
-  // expect(newEmail).toBeInTheDocument();
+  //!Assert
+  //Checks to make sure that the pre runs. If true, then all inputs are valid
+  const preRender = screen.findByLabelText(/data-display/i);
+  expect(preRender).toBeTruthy();
 });
